8336853248e4648a4a4261adabc31efd942c2012
alexmartin3103 <martinfernandezalejandro124@gmail.com>
2023-10-09 10:52:49 +0200

    Creacion de lista y recorrerla

--- P2_2_Figuras.cs
+++ P2_2_Figuras.cs
@@ -10,16 +10,43 @@ public class P2_2_Figuras
 {
   static void Main(string[] args)
   {
-    Rectangulo r = new(3, 4, Color.FromArgb(255, 50, 20, 34));
-    Circulo c = new(5, Color.FromArgb(255, 50, 20, 34));
-    TrianguloEquilatero te = new(10, Color.FromArgb(255, 50, 20, 34));
+    Rectangulo r = new(3, 3.9, Color.FromArgb(255, 50, 20, 34));
+    Circulo c = new(4, Color.FromArgb(255, 0, 20, 35));
+    TrianguloEquilatero te = new(3, Color.FromArgb(255, 0, 20, 40));
 
     Console.WriteLine(r);
-    Console.WriteLine(c);
-    Console.WriteLine(te);
     Console.WriteLine("Area:" + r.GetArea());
+    Console.WriteLine(c);
     Console.WriteLine("Area" + c.GetArea());
+    Console.WriteLine(te);
     Console.WriteLine("Area" + te.GetArea());
+
+    r.setAltura(5);
+    c.setRadio(2);
+    te.setLado(10);
+
+    Console.WriteLine(r);
+    Console.WriteLine("Area:" + r.GetArea());
+    Console.WriteLine(c);
+    Console.WriteLine("Area" + c.GetArea());
+    Console.WriteLine(te);
+    Console.WriteLine("Area" + te.GetArea());
+
+
+    List<Figura> li = new List<Figura>();
+    li.Add(new Rectangulo(3, 3.9, Color.FromArgb(255, 50, 20, 34)));
+    li.Add(new Circulo(4, Color.FromArgb(255, 0, 20, 35)));
+    li.Add(new TrianguloEquilatero(3, Color.FromArgb(255, 0, 20, 40)));
+    li.Add(r);
+    li.Add(c);
+    li.Add(te);
+
+    foreach (Figura figuras in li)
+    {
+      Console.WriteLine(figuras);
+
+    }
+
 
 
   }
